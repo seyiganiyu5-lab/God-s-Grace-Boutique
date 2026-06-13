8f03da886d4fa6279bc03700f04831a5b8fb137a
Z User <z@container>
2026-06-13 02:39:00 +0000

    1c44731f-371a-4e36-859d-8e89fe213164

--- src/components/wave-logo.tsx
+++ src/components/wave-logo.tsx
@@ -7,39 +7,14 @@ interface WaveLogoProps {
 
 export function WaveLogo({ className = '', size = 28 }: WaveLogoProps) {
   return (
-    <svg
+    // eslint-disable-next-line @next/next/no-img-element
+    <img
+      src="/wave-logo.png"
+      alt="Wave"
       width={size}
       height={size}
-      viewBox="0 0 512 512"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
       className={className}
-    >
-      <rect width="512" height="512" rx="128" fill="#1DC7EA" />
-      <path
-        d="M100 260c30-40 60-50 90-30s60 50 90 30 60-50 90-30 40 30 40 30"
-        stroke="white"
-        strokeWidth="36"
-        strokeLinecap="round"
-        fill="none"
-        opacity="0.9"
-      />
-      <path
-        d="M100 320c30-40 60-50 90-30s60 50 90 30 60-50 90-30 40 30 40 30"
-        stroke="white"
-        strokeWidth="36"
-        strokeLinecap="round"
-        fill="none"
-        opacity="0.6"
-      />
-      <path
-        d="M100 380c30-40 60-50 90-30s60 50 90 30 60-50 90-30 40 30 40 30"
-        stroke="white"
-        strokeWidth="36"
-        strokeLinecap="round"
-        fill="none"
-        opacity="0.35"
-      />
-    </svg>
+      style={{ objectFit: 'contain' }}
+    />
   );
 }
